897366704bc6947395be95946f18ce164100c65b
David <d.rehle@gmail.com>
2021-04-05 02:13:08 +0200

    hsqldb example working

---
+++ 1b-jdbc-hsqldb/readme.md
@@ -0,0 +1,13 @@
+# DareIT Challenge 1b - JDBC with external HSQLDB database
+
+## prerequisites
+
+## how to compile and run
+* `cd ???/dareit???/bin`
+* `javac -d . ../src/com/dareit/*.java ../src/com/dareit/common/*.java`
+* `java -cp ../lib/hsqldb-2.5.1.jar:. com.dareit.Main`
+
+
+## resources
+`https://examples.javacodegeeks.com/enterprise-java/sql-enterprise-java/jdbc-hsqldb-tutorial/`
+`https://stackoverflow.com/questions/9602006/hsqldb-issue-starting-the-hsql-database-from-java-code`
---
+++ 1b-jdbc-hsqldb/src/com/dareit/Main.java
@@ -0,0 +1,55 @@
+package com.dareit;
+
+import com.dareit.common.Customer;
+import com.dareit.hsqldb.Api;
+import org.hsqldb.server.Server;
+import org.hsqldb.server.ServerAcl;
+import org.hsqldb.util.DatabaseManagerSwing;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+
+public class Main {
+
+    private static final Scanner SCANNER = new Scanner(System.in);
+
+
+    public static void main(String[] args) throws IOException, ServerAcl.AclFormatException {
+        System.out.println("DareIT challenge 1 - JDBC with HSQLDB");
+
+        Server server = startServer();
+
+        Api api = Api.getInstance();
+
+        Customer newCustomer = new Customer(
+                "petr",
+                "poter"
+        );
+
+        api.createCustomer(newCustomer);
+
+        System.out.println("Data from table " + Customer.class.getSimpleName() + ":");
+        api.readCustomers().forEach(customer -> System.out.println(customer.getFirstName() + " " + customer.getLastName()));
+
+        DatabaseManagerSwing.main(new String[]{
+                "--url", "jdbc:hsqldb:mem:dareitdb", "--noexit"
+        });
+
+        server.stop();
+    }
+
+    private static Server startServer() {
+        Server server = new Server();
+        server.setDatabasePath(0, "mem:dareitdb");
+        server.setDatabaseName(0, "dareitdb");
+        server.setLogWriter(null); // can use custom writer
+        server.setErrWriter(null); // can use custom writer
+        server.setSilent(true);
+        server.setRestartOnShutdown(false);
+        server.start();
+
+        return server;
+    }
+
+}
