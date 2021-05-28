aebbb608f793ba54d95949ea55ab4423e41a00d5
David <d.rehle@gmail.com>
2021-05-28 08:14:10 +0200

    scripts working from any path

--- 1a-jdbc-mysql/scripts/compile.sh
+++ 1a-jdbc-mysql/scripts/compile.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-cd ../bin
+BASEDIR=$(realpath "$(dirname "$(readlink -f "$0")")")
+cd "$BASEDIR"/../bin
 
 javac -d . ../src/com/dareit/*.java ../src/com/dareit/common/*.java ../src/com/dareit/mysql/*.java
 
--- 1b-jdbc-hsqldb/scripts/compile.sh
+++ 1b-jdbc-hsqldb/scripts/compile.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-cd ../bin
+BASEDIR=$(realpath "$(dirname "$(readlink -f "$0")")")
+cd "$BASEDIR"/../bin
 
 javac -d . -cp ../lib/hsqldb-2.5.1.jar:. ../src/com/dareit/*.java ../src/com/dareit/common/*.java ../src/com/dareit/hsqldb/*.java
 
